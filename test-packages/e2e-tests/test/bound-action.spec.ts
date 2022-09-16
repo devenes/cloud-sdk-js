07678780367a476d3e5c6cc6d6939b492a106867
Florian Wilhelm <florian.wilhelm02@sap.com>
2022-09-16 17:29:47 +0200

    Add failing tests for bound functions actions

---
+++ test-packages/e2e-tests/test/bound-action.spec.ts
@@ -0,0 +1,22 @@
+import {
+  createTestEntityById,
+  createTestEntityByIdReturnId
+} from '@sap-cloud-sdk/test-services-e2e/v4/test-service/action-imports';
+import { destination } from './test-util';
+import { deleteEntity } from './test-utils/test-entity-operations';
+
+const entityKey = 999;
+
+describe('bound action', () => {
+  beforeEach(async () => deleteEntity(entityKey, destination));
+  afterEach(async () => deleteEntity(entityKey, destination));
+
+  it('should be able to call simple bound action', async () => {
+    const entity = await createTestEntityById({ id: entityKey })
+      .skipCsrfTokenFetching()
+      .execute(destination);
+
+    const actionResult = entity.boundActionWithoutArguments();
+    expect(actionResult.value).toBe('abc');
+  });
+});
---
+++ test-packages/e2e-tests/test/bound-function.spec.ts
@@ -0,0 +1,20 @@
+import {
+  getByKey
+} from '@sap-cloud-sdk/test-services-e2e/v4/test-service/function-imports';
+
+const url = 'http://localhost:4004/';
+const destination = { url };
+
+describe('bound functions', () => {
+  describe('integer parameter, returns entity', () => {
+    const request = getByKey({
+      param: 101
+    });
+
+    it('should execute request', async () => {
+      const entity = (await request.execute(destination));
+      const functionResult = entity.boundFunctionWithoutArguments();
+      expect(functionResult).toEqual('abc')
+    });
+  });
+});
